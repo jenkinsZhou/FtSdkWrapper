24083879e22558f0cc6e46995611d919bb1a96b3
jenkinszhou <971613168@qq.com>
2025-06-03 10:28:06 +0800

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,31 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+        flatDir {
+            dirs("ft_lib_wrapper/libs")
+        }
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        flatDir {
+            dirs("ft_lib_wrapper/libs")
+        }
+    }
+
+}
+
+rootProject.name = "FtSdkWrapper"
+include(":app")
+include(":ft_lib_wrapper")
